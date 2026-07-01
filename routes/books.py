2ded33b04f05a01c14e99b325fee9cb76d1a5c6b
apchy-ru <zz074692@google.com>
2026-07-01 20:40:26 +0300

    Added post book and created model book

--- routes/books.py
+++ routes/books.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter
+from models import Book
 
 router = APIRouter(prefix="/books", tags=["Books"])
 
@@ -8,4 +9,16 @@ books = [None]
 def get_all():
     return {
         "data": books,
+    }
+
+@router.post('/')
+def post_book(book: Book):
+    new_book = {
+        "user": '',
+        "title": book.title,
+        "text": book.text,
+    }
+
+    return {
+        "data": new_book
     }
